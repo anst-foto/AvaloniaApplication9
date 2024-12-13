baecb90ca677a3badc456e9f69e1b2e84c03f00e
Starinin Andrey <starinin-andrey@ya.ru>
2024-12-13 21:07:37 +0300

    Initial commit

---
+++ AvaloniaApplication9/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace AvaloniaApplication9.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    [Reactive] public bool IsAlarm {get; set;}
+    [Reactive] public bool IsWarning {get; set;}
+    
+    public ReactiveCommand<Unit, Unit> Command { get; }
+    
+    public bool IsFoo => IsAlarm || IsWarning;
+
+    public MainWindowViewModel()
+    {
+        IsAlarm = true;
+        IsWarning = false;
+        
+        Command = ReactiveCommand.Create(execute:() =>
+        {
+            IsAlarm = !IsAlarm;
+            IsWarning = !IsWarning;
+        }, 
+            this.WhenAnyValue(
+            p1 => p1.IsAlarm,
+            p2 => p2.IsWarning,
+            (x, y) => x || y));
+    }
+}
